905db7ed7abebae50ceab2c993384dd88d09969d
loadevent <loadevent.academy@gmail.com>
2025-08-12 07:04:29 +0200

    loading project to repo

---
+++ Jet Skills(SMS)/Jet Skills(SMS)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jet_Skills_SMS_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

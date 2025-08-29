7080239b8bf5bfded1f54e74628bd13b044f569e
loadevent <loadevent.academy@gmail.com>
2025-08-29 15:29:21 +0200

    Added Login form #11 and still need to work on the dropdown list for the user type #12

--- Jet Skills(SMS)/Jet Skills(SMS)/Controllers/LoginController.cs
+++ Jet Skills(SMS)/Jet Skills(SMS)/Controllers/LoginController.cs
@@ -5,15 +5,32 @@ namespace Jet_Skills_SMS_.Controllers
 {
     public class LoginController : Controller
     {
+        JetSkillsDbContext db = new JetSkillsDbContext();
+        public LoginController() { }
         public IActionResult Login()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Login(String username, String password)
+        public IActionResult Login(IFormCollection collection)
         {
-            return View();
-        }//0877301166
+            //List<UserType> userTypes = db.UserTypes.ToList();
+            //ViewBag.userTypes = userTypes;
+
+            int username = Convert.ToInt32(collection["username"]);
+            string password = collection["password"];
+            int userType = Convert.ToInt32(collection["userType"]);
+
+            var user = db.Students.Where(u => u.StudentId == username && u.Password == password && u.UserType == 3).FirstOrDefault();
+
+            if (user == null)
+            {
+                ViewBag.error = "Invalid Login Credentials";
+                return View();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
 
 
 
